d5fbfaf4cdf95016bea857e81eca82d9ab268bf0
Max Hoecker <hoecker.max@gmail.com>
2020-05-10 19:13:46 -0400

    framework for basic border template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import './App.css';
+import './templates/borderTemplate.css';
 import './modules/TemplateModule.js'
-import TimerModule from './modules/Time.js';
+import TimeModule from './modules/Time.js';
 
 function App() {
   return (
-    <div>
-      Main App
-      <TimerModule />
+    <div className={"container"}>
+        <div className={"header"}>
+            <TimeModule />
+        </div>
+        <div className={"sidebarLeft"}>
+            <TimeModule />
+        </div>
+        <div className={"middle"}>
+            <TimeModule />
+        </div>
+        <div className={"sidebarRight"}>
+            <TimeModule />
+        </div>
+        <div className={"footer"}>
+            <TimeModule />
+        </div>
     </div>
   );
 }
